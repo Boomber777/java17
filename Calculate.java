c91c96eed4f3411e2e05431c2cc35fc79113fa1c
is_ao1 <is_ao1>
2017-11-14 11:19:42 +0300

    next mod Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,10 +1,28 @@
+
+/*
+ sum - сумма
+
+ */
+
 public class Calculate {
 
     public static void main(String[] args) {
-        int param1 = Integer.valueOf(args[0]);
-        int param2 = Integer.valueOf(args[1]);
-        int sum = param1 + param2;
-        System.out.println(sum);
+        double param1 = Double.valueOf(args[1]);
+        double param2 = Double.valueOf(args[2]);
+        double result = 0;
+
+        if (args[0].equals("sum")) {
+            result = param1 + param2;
+        }else if(args[0].equals("dif")) {
+            result = param1 - param2;
+        }else if (args[0].equals("incr")){
+            result = param1 * param2;
+        }else if (args[0].equals("div")){
+            result = param1 / param2;
+        }else if (args[0].equals("pow")){
+            result = Math.pow(param1, param2);
+        }
+        System.out.println(result);
     }
 
 }
