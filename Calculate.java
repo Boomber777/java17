7b7bc4ac236f9d64de3d8671357b1e651b9e294c
is_ao1 <is_ao1>
2017-11-14 09:31:23 +0300

    first mod Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,7 +1,10 @@
 public class Calculate {
 
-	public static void main(String[] arg) {
-		System.out.println("Моя первая заливка в гит");
-	}
+    public static void main(String[] args) {
+        int param1 = Integer.valueOf(args[0]);
+        int param2 = Integer.valueOf(args[1]);
+        int sum = param1 + param2;
+        System.out.println(sum);
+    }
 
 }
