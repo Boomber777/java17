98103534bd8999b1c453ef5fc62faffb6670165c
is_ao1 <is_ao1>
2017-11-17 14:48:18 +0300

    готовый калькулятор!

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,7 @@
 
 /*
- sum - сумма
+ класс для вычисления арифмитических операций
+ Название операции и числа берутся из входящего массива
 
  */
 
@@ -13,16 +14,35 @@ public class Calculate {
 
         if (args[0].equals("sum")) {
             result = param1 + param2;
+        } else if (args[0].equals("dif")) {
+            result = param1 - param2;
+        } else if (args[0].equals("incr")) {
+            result = param1 * param2;
+        } else if (args[0].equals("div")) {
+            result = param1 / param2;
+        } else if (args[0].equals("pow")) {
+            result = Math.pow(param1, param2);
+        }
+        System.out.println(result);
+    }
+
+    public Double getResult(String[] args) {
+        double param1 = Double.valueOf(args[1]);
+        double param2 = Double.valueOf(args[2]);
+        double result = 0;
+
+        if (args[0].equals("sum")) {
+            result = param1 + param2;
         }else if(args[0].equals("dif")) {
             result = param1 - param2;
         }else if (args[0].equals("incr")){
             result = param1 * param2;
         }else if (args[0].equals("div")){
             result = param1 / param2;
-        }else if (args[0].equals("pow")){
+        }else if (args[0].equals("pow")) {
             result = Math.pow(param1, param2);
         }
-        System.out.println(result);
+        return result;
     }
 
 }
--- InteractRunner.java
+++ InteractRunner.java
@@ -1,2 +1,46 @@
+import java.util.Scanner;
+
+/*
+класс для запуска калькулятора, поддерживает ввод пользователей
+ */
 public class InteractRunner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            Calculator calculator = new Calculator();
+            String exit = "no";
+            String action = "no";
+
+            while (!exit.equals("yes")){
+                if (calculator.getResult() !=0){
+                    System.out.println("Использовать предыдущий результат как первый аргумент? (yes/no)");
+                    if (!scanner.next().equals("yes")){
+                        calculator.cleanResult();
+                    }
+                }
+
+                Double argFirst = 0.0;
+                if (calculator.getResult() !=0) {
+                    argFirst = calculator.getResult();
+                    calculator.cleanResult();
+                }else {
+                    System.out.println("Введите первый аргумент:");
+                    argFirst = scanner.nextDouble();
+                }
+                System.out.println("Введите второй аргумент:");
+                Double argSecond = scanner.nextDouble();
+                System.out.println("Какую операцию выполняем?(sum, dif, incr, div, pow)");
+                action = scanner.next();
+                calculator.add(action, argFirst, argSecond);
+                System.out.println(calculator.getResult());
+                System.out.println("Выйти из программы?: yes/no");
+                exit = scanner.next();
+            }
+
+
+        }finally {
+            scanner.close();
+        }
+
+    }
 }
